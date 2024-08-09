25f7af556a751ce4d29608b20816c415b0a3105e
Sean Kinread <dev@kinread.com>
2024-08-09 20:15:06 +1000

    simplify button color

--- src/common/button.variants.ts
+++ src/common/button.variants.ts
@@ -4,17 +4,12 @@ import { tv, type VariantProps } from 'tailwind-variants';
  * Standard button variant styles
  */
 export const buttonVariants = tv({
-  base: ['btn', 'no-animation', 'text-base', 'h-auto', 'min-h-fit', 'px-2', 'py-2'],
+  base: ['btn', 'text-base', 'h-auto', 'min-h-fit', 'px-2', 'py-2'],
   variants: {
     color: {
-      primary: [
-        'btn-primary focus:bg-primary-focus focus:border-primary-focus hover:bg-primary-focus',
-        'disabled:opacity-40 disabled:bg-primary disabled:text-white',
-      ],
+      primary: ['btn-primary', 'disabled:opacity-40 disabled:bg-primary disabled:text-white'],
       secondary: [
-        'btn-outline text-primary border-primary',
-        'hover:bg-primary/10 hover:border-primary hover:text-primary',
-        'focus:bg-primary/10 active:bg-primary/30',
+        'active:bg-primary/30',
         'disabled:opacity-40 disabled:border-primary disabled:text-primary disabled:bg-white',
       ],
     },
